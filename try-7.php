209d98ec6153d7745c69c4a46d1238868649710a
Suhail0206327 <suhailthechikodan01@gmail.com>
2023-09-11 09:30:52 -0400

    change variable value: name variable

--- try-7.php
+++ try-7.php
@@ -1,6 +1,6 @@
 <?php 
-$name      = 'Ivy';
-$favorites = ['Chocolate', 'Toffee', 'Fudge',];
+$name      = 'Suhail';
+$favorites = ['Lollipop' ,'Chocolate', 'Toffee', 'Fudge',];
 ?>
 <!DOCTYPE html>
 <html>
