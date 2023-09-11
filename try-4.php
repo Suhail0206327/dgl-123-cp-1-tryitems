987f2410d7251505291228eda4f1c298caba14ed
Suhail0206327 <suhailthechikodan01@gmail.com>
2023-09-11 02:23:08 -0400

    add fourth exercise

---
+++ try-4.php
@@ -0,0 +1,38 @@
+<?php 
+$best_sellers = ['Chocolate', 'Mints', 'Fudge','Bubble gum', 'Toffe', 'Jelly beans'];
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Indexed arrays</title>
+
+    <h1>
+        The Candy Store
+    </h1>
+    <h2>
+Best Sellers
+
+    </h2>
+
+    <ul>
+ <li>
+    <?php  echo $best_sellers[0]?>
+ </li>
+ <li>
+    <?php  echo $best_sellers[1]?>
+ </li>
+ <li>
+    <?php  echo $best_sellers[2]?>
+ </li>
+
+    </ul>
+</head>
+<body>
+    
+</body>
+</html>
