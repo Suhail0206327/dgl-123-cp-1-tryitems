3b358f7501d772f465efadef2c8f1f983339a2a8
Suhail0206327 <suhailthechikodan01@gmail.com>
2023-09-11 02:35:57 -0400

    add new value the liset: protien

--- try-5.php
+++ try-5.php
@@ -1,18 +1,25 @@
 <?php 
-$name  = 'Guest';
-$name  = 'Ivy';
-$price = 5;
+$nutrition = [
+    'fat'   => 38, 
+    'sugar' => 51, 
+    'salt'  => 0.25,
+];
+$nutrition['fat']   = 36;
+$nutrition['fiber'] = 2.1;
+$nutrition['protein'] = 7.3;
 ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Updating Variables</title>
+    <title>Updating Arrays</title>
     <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
     <h1>The Candy Store</h1>
-    <h2>Welcome <?php echo $name; ?></h2>
-    <p>The cost of your candy is 
-       $<?php echo $price; ?> per pack.</p>
+    <h2>Nutrition (per 100g)</h2>
+    <p>Fat:   <?php echo $nutrition['fat']; ?>%</p>
+    <p>Sugar: <?php echo $nutrition['sugar']; ?>%</p>
+    <p>Salt:  <?php echo $nutrition['salt']; ?>%</p>
+    <p>Fiber: <?php echo $nutrition['fiber']; ?>%</p>
   </body>
 </html>
